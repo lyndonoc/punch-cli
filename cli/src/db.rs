04e26145e56cdeba506fd6fecf5ef1c85d027428
lyndonoc <lyndonoc@gmail.com>
2023-04-04 23:39:31 -0400

    switched to using workspaces for monorepo use

--- cli/src/db.rs
+++ cli/src/db.rs
@@ -2,6 +2,6 @@ use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 
 pub fn create_connection() -> Result<SqliteConnection, std::io::Error> {
-    Ok(SqliteConnection::establish("punchcard.db")
+    Ok(SqliteConnection::establish("./cli/punchcard.db")
         .expect("failed to establish database connection"))
 }
